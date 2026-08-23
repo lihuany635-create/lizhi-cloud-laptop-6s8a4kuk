cc7e202ae85fc9943aa88a55810f46abead2246d
lihuany635-create <lihuany635-create@users.noreply.github.com>
2026-08-23 11:48:28 +0800

    Add private realtime device chat

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="lizhi-cloud-v3";
-const FILES=["","index.html","styles.css","theme.css","study.css","app.js","manifest.webmanifest","media-manifest.json"].map(file=>new URL(file,self.registration.scope).href);
+const CACHE="lizhi-cloud-v4";
+const FILES=["","index.html","styles.css","theme.css","study.css","chat.css","app.js","manifest.webmanifest","media-manifest.json"].map(file=>new URL(file,self.registration.scope).href);
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 self.addEventListener("activate",event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
 self.addEventListener("fetch",event=>{
